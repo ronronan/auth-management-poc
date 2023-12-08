cab3306384920d9bee16a95b7feb25e3540241c5
Ronan MOREL <ronan.morel.ext@eiffage.com>
2023-12-08 16:50:47 +0100

    feat: configure keycloak auth

--- portail/src/App.vue
+++ portail/src/App.vue
@@ -3,9 +3,15 @@
     <h1>Portail</h1>
   </header>
 
-  <main>TODO</main>
+  <main>{{ userInfo }}</main>
 </template>
 
-<script setup></script>
+<script setup>
+import { storeToRefs } from 'pinia';
+import { useUserStore } from '@/stores/user';
+
+const userStore = useUserStore();
+const { userInfo } = storeToRefs(userStore);
+</script>
 
 <style scoped></style>
